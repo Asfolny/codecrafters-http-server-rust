181a886a1c0c6720a75693e8b7870bb2322efdb8
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-14 14:24:04 +0200

    Implemented header parsing and user-agent return

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // Uncomment this block to pass the first stage
+use std::collections::HashMap;
 use std::net::{Shutdown, TcpStream, TcpListener};
 use std::io::{Write, Read};
 
@@ -17,6 +18,7 @@ fn main() {
                 let mut input = [0; 512];
                 let _ = _stream.read(&mut input[..]);
                 let parsed = input.map(|x| char::from(x));
+                let mut headers: HashMap<String, String> = HashMap::new();
                 let mut path = String::new();
                 let mut path_start = false;
 
@@ -37,11 +39,64 @@ fn main() {
                     path.push(chr);
                 }
 
+                let mut past_request_line = false;
+                let mut past_header_name = false;
+                let mut header_name = String::new();
+                let mut header_body = String::new();
+                let mut found_reset = false;
+                for chr in parsed {
+                    if !past_request_line && !found_reset && chr != '\r' && chr != '\n' {
+                        continue;
+                    }
+                    
+                    if !past_request_line && chr == '\r' {
+                        found_reset = true;
+                        continue;
+                    }
+
+                    if !past_request_line && chr == '\n' {
+                        past_request_line = true;
+                        found_reset = false; // reset, we need this for later
+                        continue;
+                    }
+
+                    if !past_header_name && chr != ':' {
+                        header_name.push(chr);
+                        continue;
+                    }
+
+                    if !past_header_name {
+                        past_header_name = true;
+                        continue;
+                    }
+
+                    if !found_reset && chr != '\r' {
+                        header_body.push(chr);
+                        continue;
+                    }
+
+                    if chr == '\r' {
+                        found_reset = true;
+                    }
+
+                    if chr == '\n' {
+                        // reset header parsing
+                        found_reset = false;
+                        past_header_name = false;
+                        header_name = header_name.trim().to_string();
+                        header_body = header_body.trim().to_string();
+                        headers.insert(header_name, header_body);
+                        header_name = String::new();
+                        header_body = String::new();
+                    }
+                }
+
                 let path_parts: Vec<&str> = path.as_str().split('/').collect();
 
                 match path_parts[1] {
                     "" => handle_index(_stream),
                     "echo" => handle_echo(_stream, path_parts),
+                    "user-agent" => handle_user_agent(_stream, headers),
                     _ => handle_not_found(_stream)
                 }
             },
@@ -62,6 +117,23 @@ fn handle_echo(mut stream: TcpStream, path_parts: Vec<&str>) {
     
     if path_parts.len() == 3 { // /echo/asd handling
         body.push_str(path_parts[2]);
+    }
+
+    let _ = stream.write(b"HTTP/1.1 200 OK\r\n");
+    let _ = stream.write(b"Content-Type: text/plain\r\n");
+    let _ = stream.write(b"Content-Length: ");
+    let _ = stream.write(body.len().to_string().as_bytes());
+    let _ = stream.write(b"\r\n\r\n");
+    let _ = stream.write(body.as_bytes());
+    let _ = stream.shutdown(Shutdown::Both).expect("shutdown call failed");
+
+}
+
+fn handle_user_agent(mut stream: TcpStream, headers: HashMap<String, String>) {
+    let mut body = &String::new();
+
+    if headers.contains_key("User-Agent") {
+        body = headers.get("User-Agent").unwrap();
     }
 
     let _ = stream.write(b"HTTP/1.1 200 OK\r\n");
